0af8f2b88e860b72413eb7255c7db922d008e44e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-12 11:36:22 +0000

    Add src/virusportal/nestedfield.py

---
+++ src/virusportal/nestedfield.py
@@ -0,0 +1,60 @@
+import json
+import requests
+
+
+class NestedField:
+
+    def __init__(self, field: str):
+        """
+
+        :param field:
+        """
+
+        self.field = field
+
+    @staticmethod
+    def endpoint():
+
+        return 'https://api.coronavirus.data.gov.uk/v1/data'
+
+    @staticmethod
+    def filters(area_code: str, area_type: str = 'ltla', area_name: str = None, date: str = None) -> str:
+        """
+
+        :param area_code:
+        :param area_type: the default value is Lower-tier local authority
+        :param area_name:
+        :param date:
+        :return:
+        """
+
+        dictionary = {'areaType': area_type, 'areaCode': area_code, 'areaName': area_name, 'date': date}
+        dictionary = ['{}={}'.format(key, value) for key, value in dictionary.items() if value is not None]
+
+        return str.join(';', dictionary)
+
+    def structure(self) -> str:
+        """
+
+        :return:
+        """
+
+        fields = {'date': 'date', self.field: self.field}
+
+        return json.dumps(obj=fields, separators=(',', ':'))
+
+    def exc(self, area_code: str):
+
+        params = {'filters': self.filters(area_code=area_code), 'structure': self.structure()}
+
+        try:
+            response = requests.get(url=self.endpoint(), params=params)
+            response.raise_for_status()
+        except requests.RequestException as err:
+            raise Exception(err)
+
+        # status check
+        if response.status_code > 204:
+            raise RuntimeError(response.text)
+
+        return response.json()
