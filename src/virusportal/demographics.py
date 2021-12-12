63efe08e82e7fd1bf7a5cf1149becb7cc75b3ea2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-12 13:50:13 +0000

    Update src/virusportal/demographics.py

--- src/virusportal/demographics.py
+++ src/virusportal/demographics.py
@@ -8,22 +8,29 @@ class Demographics:
     def __init__(self, field):
         """
 
+        :param field: the nested demographic field
         """
 
         self._field = field
+
+        # initialise the nested field reading function
         self.nested = src.virusportal.nestedfield.NestedField(field=self._field)
+
+        # the age groups
+        self._age_group = ['00_04', '05_09', '10_14', '15_19', '20_24', '25_29', '30_34', '35_39',
+                           '40_44', '45_49', '50_54', '55_59', '60_64', '65_69', '70_74', '75_79',
+                           '80_84', '85_89', '90+', 'unassigned']
 
     def exc(self, area_code):
 
+        # get the JSON data set
         blob = self.nested.exc(area_code=area_code)
 
-        supplement:pd.DataFrame = pd.json_normalize(data=blob['data'], record_path=self._field,
-                                                 meta=['date'])
+        # read it into a data frame
+        supplement:pd.DataFrame = pd.json_normalize(data=blob['data'], record_path=self._field, meta=['date'])
 
-        # ['00_04' '00_59' '05_09' '10_14' '15_19' '20_24' '25_29' '30_34' '35_39'
-        #  '40_44' '45_49' '50_54' '55_59' '60+' '60_64' '65_69' '70_74' '75_79'
-        #  '80_84' '85_89' '90+' 'unassigned']
+        # select the correct range of age group elements
+        supplement = supplement.loc[supplement.age.isin(self._age_group), :]
+        print(supplement.head())
 
-        # supplement.pivot(columns='age', values='cases')
-
-        return supplement
+        return supplement.pivot(index='date', columns='age', values='cases')
