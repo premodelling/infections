bbe0ffd7d6af173f929d76b1f9fd01b4d2e972f0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-14 17:08:58 +0000

    Add src/virusportal/query.py

---
+++ src/virusportal/query.py
@@ -0,0 +1,38 @@
+"""
+For exploring the query parameters of ...
+"""
+
+import os
+import sys
+import logging
+
+
+def main():
+
+    # fields of interest
+    fields = {'date': 'date', 'newAdmissions': 'newAdmissions'}
+
+    # demarcation of interest
+    # Data?
+    #   * area_code='RT5', area_type='nhsTrust': yes
+    example = src.virusportal.measures.Measures(fields=fields).exc(area_code='RT5', area_type='nhsTrust')
+
+    # previews
+    logger.info(example)
+    logger.info('Latest record: %s', example.date.max())
+    logger.info(example.date.unique())
+
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d', datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    # libraries
+    import src.virusportal.measures
+
+    main()
