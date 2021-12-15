867750f6a310973ff00d94a4c9b277a91df7aaf5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-15 18:38:40 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Config:
 
     def __init__(self):
@@ -21,3 +24,16 @@ class Config:
                        'VaccineRegisterPopulationByVaccinationDate': 'VaccineRegisterPopulationByVaccinationDate',
                        'newVirusTestsBySpecimenDate': 'newVirusTestsBySpecimenDate',
                        'newPCRTestsBySpecimenDate': 'newPCRTestsBySpecimenDate'}
+
+        # demographics: in progress
+        self.age_groups = ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34', '35-39', '40-44',
+                           '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80-84', '85-89', '90+']
+
+        Data = collections.namedtuple(typename='Data',
+                                      field_names=['years', 'sheet_names', 'cell_ranges', 'starts', 'endings'])
+        self.data = Data._make(([], [], ['A:W'], [], []))
+
+        FieldNames = collections.namedtuple(typename='FieldNames', field_names=['cell_ranges', 'rows', 'to_drop'])
+        self.fieldnames = FieldNames._make((['A:W'], [4], []))
+
+
