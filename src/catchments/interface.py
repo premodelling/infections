468fe6e62acc90906edcb767aa9eac8886766cb5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-20 18:22:56 +0000

    Add src/catchments/interface.py

---
+++ src/catchments/interface.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import pathlib
+import logging
+import glob
+
+
+def main():
+
+    # Do not use dask here.  Instead, use dask for the MSOA & LTLA steps.
+    for year in years:
+
+        logger.info(year)
+
+        patients = src.catchments.patients.Patients(source_path=path_patients)\
+            .read(filename='{}.csv'.format(year))
+        logger.info(patients.head())
+
+
+if __name__ == '__main__':
+
+    # paths
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # data sources paths
+    path_patients = os.path.join(root, 'warehouse', 'patients')
+    path_populations = os.path.join(root, 'warehouse', 'populations', 'msoa', 'group')
+
+    # logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    # libraries
+    import src.catchments.patients
+
+
+
+    # years
+    years = [int(pathlib.Path(filepath).stem)
+             for filepath in glob.glob(os.path.join(path_patients, '*.csv'))]
+
+    main()
