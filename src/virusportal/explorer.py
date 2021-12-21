b078e517cf69865e280f294a022451e5e239025e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-21 22:15:15 +0000

    Update src/virusportal/explorer.py

--- src/virusportal/explorer.py
+++ src/virusportal/explorer.py
@@ -6,12 +6,15 @@ for SARS-CoV-2 related data.
 import os
 import sys
 import logging
+import json
+
+import pandas as pd
 
 
 def main():
     """
     Use this small program to explore the variates/measures available via the aforementioned API.  Note
-    that data is available for the area types
+    that data is available for the area types (areaType)
 
         overview: Overview data for the United Kingdom
         nation: Nation data (England, Northern Ireland, Scotland, and Wales)
@@ -24,12 +27,26 @@ def main():
     each variate/measure is not available for all area type.
 
     There is a hidden option - NHS Trust: nhsTrust - but the data is incomplete, hence we have to rely
-    on other area types; specifically LTLA, i.e., LAD, data
+    on other area types; specifically LTLA, i.e., LAD, data.
+
+    The areaType is one of 4 authorised filters for acquiring data via the API.  In brief, the filters are
+
+        areaType: Area type as string
+        areaName: Area name as string
+        areaCode: Area Code as string
+        date: Date as string [YYYY-MM-DD]
 
     :return:
     """
 
-    # Fields of interest
+    # filters
+    area_type = 'ltla'
+    area_code='E06000022'
+    dictionary = {'areaType': area_type, 'areaCode': area_code, 'areaName': None, 'date': None}
+    dictionary = ['{}={}'.format(key, value) for key, value in dictionary.items() if value is not None]
+    filters = str.join(';', dictionary)
+
+    # fields of interest
     fields = {'date': 'date',
               'dailyCases': 'newCasesBySpecimenDate',
               'dailyAdmissions': 'newAdmissions',
@@ -42,17 +59,21 @@ def main():
               'VaccineRegisterPopulationByVaccinationDate': 'VaccineRegisterPopulationByVaccinationDate',
               'newVirusTestsBySpecimenDate': 'newVirusTestsBySpecimenDate',
               'newPCRTestsBySpecimenDate': 'newPCRTestsBySpecimenDate'}
+    structure = json.dumps(obj=fields, separators=(',', ':'))
 
-    # Demarcations of interest
-    #
-    # Example
-    #   Case LTLA: area_code = 'E06000022'
-    #
-    example = src.virusportal.measures.Measures(fields=fields).exc(area_code='E06000022')
+    # url
+    url = 'https://api.coronavirus.data.gov.uk/v1/data?filters={filters}&structure={structure}&format={format}'
+    url = url.format(filters=filters, structure=structure, format='csv')
+
+    # read the data of interest via the constructed API URL
+    try:
+        frame = pd.read_csv(filepath_or_buffer=url)
+    except RuntimeError as err:
+        raise Exception(err)
 
     # previews
-    logger.info(example)
-    logger.info('Latest record: %s', example.date.max())
+    logger.info(frame)
+    logger.info('Latest record: %s', frame.date.max())
 
 
 if __name__ == '__main__':
@@ -65,8 +86,5 @@ if __name__ == '__main__':
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
-
-    # libraries
-    import src.virusportal.measures
 
     main()
