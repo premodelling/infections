1d000a39f1ce28c836479bc33da9812c143d1afe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-22 01:23:23 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -5,24 +5,13 @@ import logging
 
 def main():
 
-    # set-up parallel reading via dask; parallel by LTLA/LAD area code
+    # a LTLA/LAD area code
     area_code = 'E06000022'
-
-    # un-nested measures of interest are declared in the config.py file
-    fields = config.Config().fields
-
-    # reading-in the un-nested measures
-    frame = src.virusportal.measures.Measures(fields=fields).exc(area_code=area_code)
-    logger.info(frame.head())
 
     # reading-in the nested demographic data
     supplement = src.virusportal.demographics.Demographics(field='newCasesBySpecimenDateAgeDemographics')\
         .exc(area_code=area_code)
     logger.info(supplement.head())
-
-    # area codes for England
-    area_codes = src.ingest.areacodes.AreaCodes().exc()
-    logger.info(area_codes.head())
 
 
 if __name__ == '__main__':
@@ -37,10 +26,6 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     # libraries
-    import config
-    import src.virusportal.measures
     import src.virusportal.demographics
-
-    import src.ingest.areacodes
 
     main()
