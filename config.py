e3605109bf1b31c6c6574c53defe074688339312
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-22 15:56:28 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import collections
 import os
-import pandas as pd
 
 import numpy as np
+import pandas as pd
 
 
 class Config:
@@ -12,20 +11,28 @@ class Config:
         https://coronavirus.data.gov.uk/details/developers-guide/main-api#query-parameters
         """
 
-        self.fields = {'date': 'date',
-                       'dailyCases': 'newCasesBySpecimenDate',
-                       'newDeaths28DaysByDeathDate': 'newDeaths28DaysByDeathDate',
-                       'dailyFirstDoseByVaccinationDate': 'newPeopleVaccinatedFirstDoseByVaccinationDate',
-                       'dailySecondDoseByVaccinationDate': 'newPeopleVaccinatedSecondDoseByVaccinationDate',
-                       'dailyThirdInjectionByVaccinationDate': 'newPeopleVaccinatedThirdInjectionByVaccinationDate',
-                       'VaccineRegisterPopulationByVaccinationDate': 'VaccineRegisterPopulationByVaccinationDate',
-                       'newVirusTestsBySpecimenDate': 'newVirusTestsBySpecimenDate',
-                       'newPCRTestsBySpecimenDate': 'newPCRTestsBySpecimenDate'}
+        # fields
+        self.fields_ltla = {'date': 'date',
+                            'dailyCases': 'newCasesBySpecimenDate',
+                            'newDeaths28DaysByDeathDate': 'newDeaths28DaysByDeathDate',
+                            'dailyFirstDoseByVaccinationDate': 'newPeopleVaccinatedFirstDoseByVaccinationDate',
+                            'dailySecondDoseByVaccinationDate': 'newPeopleVaccinatedSecondDoseByVaccinationDate',
+                            'dailyThirdInjectionByVaccinationDate': 'newPeopleVaccinatedThirdInjectionByVaccinationDate',
+                            'VaccineRegisterPopulationByVaccinationDate': 'VaccineRegisterPopulationByVaccinationDate',
+                            'newVirusTestsBySpecimenDate': 'newVirusTestsBySpecimenDate',
+                            'newPCRTestsBySpecimenDate': 'newPCRTestsBySpecimenDate'}
 
+        self.fields_trust = {'date': 'date',
+                             'covidOccupiedBeds': 'hospitalCases',
+                             'covidOccupiedMVBeds': 'covidOccupiedMVBeds',
+                             'estimatedNewAdmissions': 'newAdmissions'}
+
+        # age groups
         self.age_groups = ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34', '35-39', '40-44',
                            '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80-84', '85-89', '90+']
         self.age_group_length = 5
 
+        # ages
         ages = list(np.arange(start=0, stop=90))
         self.ages = ages + ['90+']
         self.ages_length = len(ages)
@@ -47,3 +54,19 @@ class Config:
         frame.rename({'MSOA11CD': 'msoa', 'LAD20CD': 'ltla'}, axis=1, inplace=True)
 
         return frame
+
+    @staticmethod
+    def trusts():
+
+        uri = os.path.join('data', 'catchment', '2021 Trust Catchment Populations_Supplementary Trust Area lookup.xlsx')
+        sheet_name = 'Trust Area Lookup'
+        rename = {'TrustCode': 'trust_code', 'TrustName': 'trust_name'}
+
+        try:
+            frame = pd.read_excel(io=uri, sheet_name=sheet_name, header=0, usecols=['TrustCode', 'TrustName'])
+        except RuntimeError as err:
+            raise Exception(err)
+        frame.rename(columns=rename, inplace=True)
+
+        return frame
+
