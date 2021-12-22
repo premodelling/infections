021aa661fa763127ac405d36aaa0ad3ab15f2dc0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-22 01:23:11 +0000

    Update src/virusportal/measures.py

--- src/virusportal/measures.py
+++ src/virusportal/measures.py
@@ -1,4 +1,9 @@
+import collections
 import json
+import os.path
+
+import dask
+
 import pandas as pd
 
 
@@ -9,58 +14,53 @@ class Measures:
 
         :param fields:
         """
+        
+        # the API endpoint 
+        self.endpoint = 'https://api.coronavirus.data.gov.uk/v1/data'
+        
+        # the structure object of the API fields of interest
+        self.structure = json.dumps(obj=fields, separators=(',', ':'))
 
-        self.fields = fields
+        # storage
+        self.storage = os.path.join(os.getcwd(), 'warehouse', 'virus', 'ltla')
+        self.__path()
 
-    @staticmethod
-    def url():
+    def __path(self):
         """
 
         :return:
         """
 
-        endpoint = 'https://api.coronavirus.data.gov.uk/v1/data'
+        if not os.path.exists(self.storage):
+            os.makedirs(self.storage)
 
-        return endpoint + '?filters={filters}&structure={structure}&format={format}'
-
-    @staticmethod
-    def filters(area_code: str, area_type: str, area_name: str, date: str) -> str:
+    @dask.delayed
+    def __url(self, parameters: collections.namedtuple):
         """
 
-        :param area_code:
-        :param area_type:
-        :param area_name:
-        :param date:
+        :param parameters:
         :return:
         """
 
-        dictionary = {'areaType': area_type, 'areaCode': area_code, 'areaName': area_name, 'date': date}
+        dictionary = {'areaType': parameters.area_type,
+                      'areaCode': parameters.area_code,
+                      'areaName': parameters.area_name,
+                      'date': parameters.date}
         dictionary = ['{}={}'.format(key, value) for key, value in dictionary.items() if value is not None]
+        filters = str.join(';', dictionary)
 
-        return str.join(';', dictionary)
+        url = self.endpoint + '?filters={filters}&structure={structure}&format={format}'
+        url = url.format(filters=filters, structure=self.structure, format='csv')
 
-    def structure(self) -> str:
+        return url
+
+    @dask.delayed
+    def __read(self, url):
         """
 
+        :param url:
         :return:
         """
-
-        return json.dumps(obj=self.fields, separators=(',', ':'))
-
-    def exc(self, area_code: str, area_type: str = 'ltla', area_name: str = None, date: str = None) -> pd.DataFrame:
-        """
-
-        :param area_code:
-        :param area_type: the default value is Lower-tier local authority
-        :param area_name:
-        :param date:
-        :return:
-        """
-
-        url = self.url().format(
-            filters=self.filters(area_code=area_code, area_type=area_type, area_name=area_name, date=date),
-            structure=self.structure(), format='csv'
-        )
 
         try:
             frame = pd.read_csv(filepath_or_buffer=url)
@@ -68,3 +68,39 @@ class Measures:
             raise Exception(err)
 
         return frame
+
+    @dask.delayed
+    def __write(self, frame: pd.DataFrame, parameters: collections.namedtuple) -> str:
+        """
+
+        :param frame:
+        :param parameters:
+        :return:
+        """
+
+        try:
+            frame.to_csv(path_or_buf=os.path.join(self.storage, '{}.csv'.format(parameters.area_code)),
+                         index=False, header=True, encoding='utf-8')
+            return '{}: succeeded'.format(parameters.area_code)
+        except RuntimeError as err:
+            raise Exception(err)
+
+    def exc(self, parameters_: list):
+        """
+
+        :param parameters_:
+        :return:
+        """
+
+        computations = []
+        for parameters in parameters_:
+
+            url = self.__url(parameters=parameters)
+            frame = self.__read(url=url)
+            message = self.__write(frame=frame, parameters=parameters)
+            computations.append(message)
+
+        dask.visualize(computations, filename='measures', format='pdf')
+        messages = dask.compute(computations, scheduler='processes')[0]
+
+        return messages
