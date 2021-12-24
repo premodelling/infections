d19480b93f26b8026a17ee20ab445a4232b75e76
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-24 11:52:19 +0000

    Update src/weights/ltla.py

--- src/weights/ltla.py
+++ src/weights/ltla.py
@@ -1,60 +1,80 @@
-"""
-This program collates the LTLA/Trust calculations; per trust, each year's LTLA/Trust weights
-are saved in a single file.
-
-"""
-
 import glob
 import logging
 import os
 import pathlib
-import sys
 
 import dask.dataframe
 import numpy as np
 
 
-def main():
+class LTLA:
 
-    # the distinct source file names
-    names = np.array([pathlib.Path(file).stem for file in files])
-    names = np.unique(names)
+    def __init__(self):
+        """
+        
+        """
 
-    # per trust, this snippet will search for the trust's files across all years, merge
-    # the data, then save
-    for name in names:
-        logger.info(name)
+        self.variables_aggregated = ['year', 'ltla', 'ppln_ltla', 'patients_from_ltla_to_trust',
+                                     'total_patients_of_ltla', 'tfp_ltla', 'etc_ltla', 'total_trust_patients',
+                                     'ltla_frac_tp']
 
-        # search
-        # members = glob.glob(os.path.join(source, '*', '{}.csv'.format(name)), recursive=True)
+        # the source files
+        self.source_path = os.path.join(os.getcwd(), 'warehouse', 'trusts', 'weights', 'segments', 'ltla')
+        self.source_files = glob.glob(os.path.join(self.source_path, '*', '*.csv'), recursive=True)
 
-        # search for and read all the trust's files at once
-        frame = dask.dataframe.read_csv(urlpath=os.path.join(source, '*', '{}.csv'.format(name)))
+        # storage
+        path = os.path.join(os.getcwd(), 'warehouse', 'trusts', 'weights', 'series', 'ltla')
+        self.path_disaggregated = os.path.join(path, 'disaggregated')
+        self.path_aggregated = os.path.join(path, 'aggregated')
+        self.__path(paths = [self.path_aggregated, self.path_disaggregated])
 
-        # save the trust's data in a single file
-        dask.dataframe.to_csv(df=frame,
-                              filename=os.path.join(storage, '{}.csv'.format(name)),
-                              single_file=True, index=False)
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.logger = logging.getLogger(__name__)
 
+    def __path(self, paths: list):
+        """
 
-if __name__ == '__main__':
-    root = os.getcwd()
-    sys.path.append(root)
-    sys.path.append(os.path.join(root, 'src'))
+        :return:
+        """
+        for path in paths:
+            if not os.path.exists(path):
+                os.makedirs(path)
 
-    # the source files
-    source = os.path.join(root, 'warehouse', 'trusts', 'weights', 'segments', 'ltla')
-    files = glob.glob(os.path.join(source, '*', '*.csv'), recursive=True)
+    def __source_names(self):
+        """
+        A Trust ca have data within the directory of one or more years.  This function/method
+        returns unique trust names.
 
-    # storage
-    storage = os.path.join('warehouse', 'trusts', 'weights', 'series', 'ltla')
-    if not os.path.exists(storage):
-        os.makedirs(storage)
+        :return:
+        """
 
-    # Logging
-    logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(__name__)
+        # the distinct source file names
+        names = np.array([pathlib.Path(source_file).stem for source_file in self.source_files])
+        return np.unique(names)
 
-    main()
+    def exc(self):
+
+        source_names = self.__source_names()
+
+        # per trust, this snippet will search for the trust's files across all years, merge
+        # the data, then save
+        for source_name in source_names:
+
+            # source name
+            self.logger.info(source_name)
+
+            # search for and read all the trust's files at once
+            frame = dask.dataframe.read_csv(urlpath=os.path.join(self.source_path, '*', '{}.csv'.format(source_name)))
+
+            # save the trust's data in a single file
+            dask.dataframe.to_csv(df=frame,
+                                  filename=os.path.join(self.path_disaggregated, '{}.csv'.format(source_name)),
+                                  single_file=True, index=False)
+
+            reduced = frame[self.variables_aggregated].drop_duplicates()
+            dask.dataframe.to_csv(df=reduced,
+                                  filename=os.path.join(self.path_aggregated, '{}.csv'.format(source_name)),
+                                  single_file=True, index=False)
