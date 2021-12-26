7c079f03c29a0793faf66df40aa99f897de9af7a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-26 23:42:34 +0000

    Update src/design/interface.py

--- src/design/interface.py
+++ src/design/interface.py
@@ -2,37 +2,11 @@ import logging
 import os
 import sys
 
-import pandas as pd
-
 
 def main():
 
-    # per trust
-    trust_codes = trusts.trust_code.unique()
-    for trust_code in trust_codes[2:4]:
-
-        logger.info(trust_code)
-
-        # trust data
-        data = src.design.trustdata.TrustData().exc(trust_code=trust_code)
-
-        # trust cases per age group determined via weights& LTLA cases
-        disaggregated = src.design.disaggregatedCases.DisaggregatedCases(trust_code=trust_code).exc()
-
-        # weighted aggregated cases
-        cases = src.design.aggregatedMeasures.AggregatedMeasures().exc(trust_code=trust_code, field='dailyCases')
-
-        # weighted vaccination numbers
-        first = src.design.aggregatedMeasures.AggregatedMeasures()\
-            .exc(trust_code=trust_code, field='dailyFirstDoseByVaccinationDate')
-
-        second = src.design.aggregatedMeasures.AggregatedMeasures()\
-            .exc(trust_code=trust_code, field='dailySecondDoseByVaccinationDate')
-
-        # finally, the design matrix for the ML/forecasting algorithms
-        frame = pd.concat((data, disaggregated, cases, first, second), axis=1, ignore_index=False)
-        logger.info(frame.info())
-        logger.info(frame.head())
+    messages = src.design.matrix.Matrix().exc()
+    logger.info(messages)
 
 
 if __name__ == '__main__':
@@ -47,13 +21,6 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     # libraries
-    import config
-    import src.design.disaggregatedCases
-    import src.design.aggregatedMeasures
-    import src.design.trustdata
-
-    # configurations
-    configurations= config.Config()
-    trusts = configurations.trusts()
+    import src.design.matrix
 
     main()
