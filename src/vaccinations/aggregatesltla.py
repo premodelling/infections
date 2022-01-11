3febba510acdfb38d309ab18e65da1f63481ed50
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-11 11:23:00 +0000

    Update src/vaccinations/aggregatesltla.py

--- src/vaccinations/aggregatesltla.py
+++ src/vaccinations/aggregatesltla.py
@@ -14,8 +14,9 @@ class AggregatesLTLA:
         self.patients = patients
         self.populations = populations
 
+        # age groups
         self.age_groups = ['12-15', '16-17', '18-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54',
-                           '55-59', '60-64', '65-69', '70-74', '75-79', '80-84', '85-89']
+                           '55-59', '60-64', '65-69', '70-74', '75-79', '80-84', '85-89', '90+']
 
     def __patients_to_trust(self) -> pd.DataFrame:
         """
