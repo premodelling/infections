bff88fc109554eec4195eea20b6e63c94b4952a6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-13 21:44:31 +0000

    Add src/prototyping/Settings.py

---
+++ src/prototyping/Settings.py
@@ -0,0 +1,36 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class Settings:
+    """
+    Class Settings: For Jupyter Lab notebooks
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+    @staticmethod
+    def aesthetics():
+        """
+
+        :return:
+        """
+
+        matplotlib.rcParams.update({'font.size': 13})
+        matplotlib.rcParams['text.usetex'] = False
+
+        sns.set(font_scale=1)
+        sns.axes_style('whitegrid')
+
+    @staticmethod
+    def layout():
+        """
+
+        :return:
+        """
+
+        plt.rcParams['figure.constrained_layout.use'] = False
