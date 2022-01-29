a5a01e35b799d51b18952d43fe9dfccbb959eff9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-29 19:10:18 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -2,21 +2,10 @@ import logging
 import os
 import sys
 
-import collections
-
 
 def main():
 
     logger.info('infections')
-
-    # the training, validating, and testing data sets
-    Fraction = collections.namedtuple(typename='Fraction', field_names=['training', 'validating', 'testing'])
-    training, validating, testing = src.modelling.DataStreams.DataStreams(
-        root=root, fraction=Fraction._make((0.75, 0.15, 0.10))).exc()
-
-    logger.info('training data %s', training.shape)
-    logger.info('validating data %s', validating.shape)
-    logger.info('testing data %s', testing.shape)
 
 
 if __name__ == '__main__':
@@ -30,8 +19,5 @@ if __name__ == '__main__':
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
-
-    # libraries
-    import src.modelling.DataStreams
 
     main()
