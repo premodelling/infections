affd2b0dbce87204fe3aec5fff393752d744a0e9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-30 00:16:27 +0000

    Update src/prototyping/DataNormalisation.py

--- src/prototyping/DataNormalisation.py
+++ src/prototyping/DataNormalisation.py
@@ -8,6 +8,9 @@ class DataNormalisation:
     def __init__(self, training: pd.DataFrame, validating: pd.DataFrame, testing: pd.DataFrame):
         """
 
+        :param training:
+        :param validating:
+        :param testing:
         """
 
         self.__training = training
@@ -18,10 +21,20 @@ class DataNormalisation:
         self.__deviation = self.__training.std(axis=0)
 
     def __normalise(self, data: pd.DataFrame):
+        """
+
+        :param data:
+        :return:
+        """
 
         return (data - self.__mean) / self.__deviation
 
     def graph(self, data):
+        """
+
+        :param data:
+        :return:
+        """
 
         normalised = self.__normalise(data)
         melted = normalised.melt(var_name='Field', value_name='Normalised')
@@ -34,6 +47,10 @@ class DataNormalisation:
         handle.set_xticklabels(normalised.keys(), rotation=90)
 
     def exc(self):
+        """
+
+        :return:
+        """
         
         training = self.__normalise(data=self.__training)
         validating = self.__normalise(data=self.__validating)
