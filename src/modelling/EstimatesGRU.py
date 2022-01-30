299521287c5b89b6506001d9cf24b48fa55a3c63
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-01-30 03:22:49 +0000

    Add src/modelling/EstimatesGRU.py

---
+++ src/modelling/EstimatesGRU.py
@@ -0,0 +1,81 @@
+import os
+
+import pandas as pd
+import tensorflow as tf
+
+import src.modelling.ModellingSteps
+import src.modelling.WindowGenerator
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+class EstimatesLSTM:
+
+    def __init__(self, n_features: int, output_steps: int):
+        """
+
+        :param n_features:
+        :param output_steps:
+        """
+
+        self.n_features = n_features
+        self.output_steps = output_steps
+
+        # an instance of modelling steps
+        # trial mode, hence the low number of epochs
+        self.steps = src.modelling.ModellingSteps.ModellingSteps(epochs=5)
+
+        # method
+        self.method = 'GRU'
+
+        # storage
+        self.storage = os.path.join(os.getcwd(), 'warehouse', 'modelling', 'evaluations', 'histories', self.method)
+        self.__path()
+
+    def __path(self):
+
+        if not os.path.exists(self.storage):
+            os.makedirs(self.storage)
+
+    def __write(self, blob: pd.DataFrame, width: int):
+        """
+
+        :param blob:
+        :param width:
+        :return:
+        """
+
+        try:
+            blob.to_csv(path_or_buf=os.path.join(self.storage, '{:03d}.csv'.format(width)),
+                        index=False, header=True)
+        except RuntimeError as err:
+            raise Exception(err)
+
+    def exc(self, width: int, window: src.modelling.WindowGenerator.WindowGenerator):
+        """
+
+        :param width:
+        :param window:
+        :return:
+        """
+
+        gru = tf.keras.Sequential([
+            tf.keras.layers.GRU(32, input_shape=(width, self.n_features), return_sequences=True, activation='relu',
+                                kernel_initializer=tf.initializers.HeUniform()),
+            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.GRU(16, return_sequences=True, activation='relu'),
+            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.GRU(self.output_steps * self.n_features, activation='relu'),
+            tf.keras.layers.Reshape([self.output_steps, self.n_features])
+        ])
+
+        gru_ = self.steps.modelling(model=gru, window=window)
+
+        gru_history = pd.DataFrame(data=gru_.history)
+        gru_history.loc[:, 'method'] = self.method
+        gru_history.loc[:, 'history'] = width
+        gru_history.loc[:, 'ahead'] = self.output_steps
+
+        self.__write(blob=gru_history, width=width)
+
+        return gru_, self.method
